1363e140893edee2caf8e905f0803272d34008de
Dianochka Solovyova <56130345+disvik@users.noreply.github.com>
2022-12-21 20:02:27 +0200

    Delete password_id_correct_v3.sql

--- Laboratory-work-10/password_id_correct_v3.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE FUNCTION password_is_correct_v3(password varchar)
-RETURNS BOOLEAN
-AS $$
-BEGIN
-	RETURN password NOT SIMILAR TO '(\S{11,20}|[^0-9]{1,10}|[^a-z]{2,10}|[^A-Z]{4,10}|[^!@#$%^&*]{2,10})';
-END;
-$$ LANGUAGE plpgsql;
